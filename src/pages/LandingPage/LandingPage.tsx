bd245d6d58b1c6b841a694dddbe8a04d5ffeee50
OGisidore <isidoreogoulodo43@gmail.com>
2024-07-28 16:36:30 +0100

    first commit

---
+++ src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import styles from './LandingPage.module.css';
+
+interface LandingPageProps {}
+
+const LandingPage: FC<LandingPageProps> = () => (
+
+  <div className='container flex flex-col justify-center h-[100vh]'>
+    LandingPage Component
+  </div>
+);
+
+export default LandingPage;
