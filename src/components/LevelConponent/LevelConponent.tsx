8084917137318a7ac7599a6d473832eb3ebbfae3
OGisidore <isidoreogoulodo43@gmail.com>
2024-07-29 15:11:06 +0100

    single correction into level sentences

--- src/components/LevelConponent/LevelConponent.tsx
+++ src/components/LevelConponent/LevelConponent.tsx
@@ -5,7 +5,7 @@ interface LevelConponentProps {}
 
 const LevelConponent: FC<LevelConponentProps> = () => (
   <div className=' w-[95%] flex items-center justify-between'>
-    <div className="level text-[1.5     rem]">
+    <div className="level text-[2rem]">
       Level : Beginer
     </div>
     <div className="checked flex w-[10rem] ">
