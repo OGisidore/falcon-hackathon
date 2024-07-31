323dd7a21841827c350019cb03f3d38042f804a7
OGisidore <isidoreogoulodo43@gmail.com>
2024-07-31 17:11:41 +0100

    ..

--- src/components/WebcamComponent/WebcamComponent.tsx
+++ src/components/WebcamComponent/WebcamComponent.tsx
@@ -39,7 +39,7 @@ const addCaptureimge = ()=>{
    <div className='w-[90%]  mt-[5rem] flex flex-col items-center justify-evenly  min-h-[50vh]  p-1 bg-[#156082] '>
     {
       imgSrc.length ?<> <img src={imgSrc} alt="capture-objet" /><div className="capture_btn p-[1rem] w-[20rem] bg-[#e97813] border border-solid border-black rounded-sm" onClick={()=>addCaptureimge()} > Submit   </div>
- </>  :<> <Webcam videoConstraints={videoConstraints}  audio={false} width={60} height={720} screenshotFormat="image/jpeg" className=' m-0 border border-solid border-black w-[400px]'ref={webcamref}/> <div className="capture_btn p-[3rem] cursor-pointer bg-white rounded-full" onClick={()=>capture()}>   </div></>
+ </>  :<> <Webcam videoConstraints={videoConstraints}  audio={false} width={60} height={720} screenshotFormat="image/jpeg" className=' m-0 border border-solid border-black 'ref={webcamref}/> <div className="capture_btn p-[3rem] cursor-pointer bg-white rounded-full" onClick={()=>capture()}>   </div></>
     }
     
     
