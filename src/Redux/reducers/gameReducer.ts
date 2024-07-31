d3a94b66dbccad46f179913f8a80bd1ac0d78e41
OGisidore <isidoreogoulodo43@gmail.com>
2024-07-31 16:36:58 +0100

    ..

--- src/Redux/reducers/gameReducer.ts
+++ src/Redux/reducers/gameReducer.ts
@@ -1,19 +1,10 @@
 
 import { UnknownAction } from "@reduxjs/toolkit";
-import { Clue } from "../../Helpers/Models/Clue";
 import { ADD_TO_CATEGORY, REMOVE_FROM_CATEGORY } from "../actions/ActionType";
 import { CategoryAction } from "../actions/types";
-import { generateID } from "../../Helpers/Utilities";
 
 
-// const category: Clue ={
-//     _id: generateID(),
-//     type : "Color",
-//     img_url : "",
-//     created_at : new Date(),
-//     completed : false
-// }
-// let category
+
   
 const initState: any =  {};
 export const gameReducers = (state = initState,
@@ -30,21 +21,16 @@ export const gameReducers = (state = initState,
                     const existing = state[action.key+""]
                         .find((exist: any) => exist._id === action.payload?._id)
                     if (!existing) {
-                        // console.log(action.payload);
                         action.payload.created_at = new Date()
 
 
-                        // state[action.key+""].push(action.payload)
                         state = {...state , [action.key+""]: [...[action.key+""] , action.payload]}
-                        // state[action.key+""] = [...state[action.key+""]]
                     } else {
                         action.payload.updated_at = new Date()
                         const existingIndex = state[action.key+""]
                             .findIndex((exist: any) => exist._id === action.payload?._id)
-                        // state[action.key+""][existingIndex] = action.payload
                         state = {...state , [action.key+""]: [...[action.key+""] ,[action.key+""][existingIndex] = action.payload]}
 
-                        // state[action.key+""] = [...state[action.key+""]]
                     }
                 } else {
                     state ={ ...state, [action.key+""] : action.payload}
@@ -56,7 +42,6 @@ export const gameReducers = (state = initState,
 
             return { ...state }
 
-            break;
         case REMOVE_FROM_CATEGORY:
             if (action.key) {
                 if (!action.unique) {
@@ -79,74 +64,8 @@ export const gameReducers = (state = initState,
 
             }
             return { ...state }
-            break;
 
         default:
             return state
-            break;
     }
 }
-// import { UnknownAction } from "@reduxjs/toolkit";
-// import { Clue } from "../../Helpers/Models/Clue";
-// import { ADD_TO_CATEGORY, REMOVE_FROM_CATEGORY } from "../actions/ActionType";
-// import { CategoryAction } from "../actions/types";
-// import { generateID } from "../../Helpers/Utilities";
-
-// const initState: any = {};
-
-// export const categoryReducers = (state = initState, action: UnknownAction | CategoryAction = { type: null, key: null, payload: null }) => {
-//     switch (action.type) {
-//         case ADD_TO_CATEGORY:
-//             if (action.key) {
-//                 if (!action.unique) {
-//                     const existingCategory = state[action.key + ""] || [];
-//                     const existingIndex = existingCategory.findIndex((exist: any) => exist._id === action.payload?._id);
-                    
-//                     if (existingIndex === -1) {
-//                         return {
-//                             ...state,
-//                             ...action.payload,
-//                         };
-//                     } else {
-//                         const updatedItem = {
-//                             ...action.payload,
-//                             updated_at: new Date(),
-//                         };
-//                         const updatedCategory = [...existingCategory];
-//                         updatedCategory[existingIndex] = updatedItem;
-//                         return {
-//                             ...state,
-//                              updatedCategory,
-//                         };
-//                     }
-//                 } else {
-//                     return {
-//                         ...state,
-//                         [action.key + ""]: action.payload,
-//                     };
-//                 }
-//             } else {
-//                 return action.payload;
-//             }
-
-//         case REMOVE_FROM_CATEGORY:
-//             if (action.key) {
-//                 if (!action.unique) {
-//                     const existingCategory = state[action.key + ""] || [];
-//                     const updatedCategory = existingCategory.filter((data: any) => data._id !== action.payload?._id);
-
-//                     return {
-//                         ...state,
-//                         [action.key + ""]: updatedCategory,
-//                     };
-//                 } else {
-//                     const { [action.key + ""]: removedKey, ...newState } = state;
-//                     return newState;
-//                 }
-//             }
-//             return state;
-
-//         default:
-//             return state;
-//     }
-// };
