fde8f222499ce538193dcf84ea69d62a56ccd4ea
OGisidore <isidoreogoulodo43@gmail.com>
2024-08-05 11:52:58 +0100

    home page correction

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -9,15 +9,14 @@ const Hero: FC<HeroProps> = () => {
   // const [currentGame, setCurrentGame]=useState<Game|null>();
 
  return <div className='container min-h-[100vh] md:w-[90vw] max-w-[600px] flex flex-col items-center bg-[#156082] md:h-[90vh]' >
-    <h1> CHOOSE A GAME </h1>
+  <img src="assets/images/Find and Speak logo.svg" alt="logo" />
     <div className="games">
-      {
-        Games.map((game : Game)=>{
-           return <Link to={"/" + game.slug} className="game cursor-pointer  w-fit md:w-[20rem] h-[10rem] bg-white box-shadow mt-[1rem]">
-        <img src={game.img_url} className='w-[100%] h-[100%]  ' alt="can you find it" />
+  
+           return <Link to={"/CYFI"} className="game cursor-pointer  w-fit md:w-[20rem] h-[10rem] bg-white box-shadow mt-[1rem]">
+        <img src="assets/images/FnS Page Image.svg" className='w-[100%] h-[100%]  ' alt="can you find it" />
       </Link>
-        })
-      }
+       
+      
 
      
 
