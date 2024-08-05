c41cfc2a600cef1a26039d5defa8ab30ac2f6216
OGisidore <isidoreogoulodo43@gmail.com>
2024-08-05 11:11:56 +0100

    model connected

--- src/components/ImageSubmitComponent/ImageSubmitComponent.tsx
+++ src/components/ImageSubmitComponent/ImageSubmitComponent.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import { getImageShot } from '../../Redux/Selector/Selectors';
 import { useNavigate } from 'react-router-dom';
@@ -11,16 +11,35 @@ const ImageSubmitComponent: FC<ImageSubmitComponentProps> = () => {
   
   const navigate = useNavigate()
 
-  const handleCompareImage = ()=>{
-    navigate("/step3")
-
-
-  }
+ 
+    const handleCompareImage = useCallback((imageSrc: string) => {
+      
+    
+      // Envoyer l'image au backend via fetch
+      fetch('http://localhost:5000/api/step1', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ image: imageSrc }),
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Response from backend:', data);
+        navigate("/step3");
+      })
+      .catch(error => {
+        console.error('Error submitting image:', error);
+      });
+    
+    }, [navigate]);
+    
+   
   return <div className='w-[90%]   flex flex-col items-center justify-evenly  min-h-[50vh]  p-1 bg-[#156082]  '>
   <div className="preview_image">
     <img src={imgSrc} className='max-w-[100%]' alt="preview" />
   </div>
-  <div onClick={handleCompareImage} className="submit_button border-[.2rem] border-solid border-black  rounded-[.5rem] text-white font-bold p-[1rem] w-[8rem] flex items-center justify-center text-center text-[1.3rem] bg-[#e97813]">Submit </div>
+  <div onClick={()=>handleCompareImage(imgSrc)} className="submit_button border-[.2rem] border-solid border-black  rounded-[.5rem] text-white font-bold p-[1rem] w-[8rem] flex items-center justify-center text-center text-[1.3rem] bg-[#e97813]">Submit </div>
 </div>
 };
 
