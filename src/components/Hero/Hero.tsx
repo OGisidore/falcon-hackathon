83f020b909846b2ba9028636a1f8ca33a7e6af2f
OGisidore <isidoreogoulodo43@gmail.com>
2024-08-05 14:10:06 +0100

    first sreen correct

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,4 @@
 import React, { FC, } from 'react';
-import { Game } from '../../Helpers/Models/Game';
-import { Games } from '../../Helpers/Templates/Games';
 import { Link } from 'react-router-dom';
 
 interface HeroProps {}
@@ -9,11 +7,12 @@ const Hero: FC<HeroProps> = () => {
   // const [currentGame, setCurrentGame]=useState<Game|null>();
 
  return <div className='container min-h-[100vh] md:w-[90vw] max-w-[600px] flex flex-col items-center bg-[#156082] md:h-[90vh]' >
-  <img src="assets/images/Find and Speak logo.svg" alt="logo" />
+  <img className='h-[10rem]' src="assets/images/Find and Speak logo.svg" alt="logo" />
     <div className="games">
   
-           return <Link to={"/CYFI"} className="game cursor-pointer  w-fit md:w-[20rem] h-[10rem] bg-white box-shadow mt-[1rem]">
-        <img src="assets/images/FnS Page Image.svg" className='w-[100%] h-[100%]  ' alt="can you find it" />
+        <Link to={"/FnS"} className="game cursor-pointer flex justify-center flex-col items-center  w-fit md:w-[20rem] h-[100%]  mt-[1rem]">
+        <img src="assets/images/FnS Page Image.svg" className='w-[100%]   ' alt="can you find it" />
+        <div  className="btn flex justify-center p-0 mt-0 "> <span className='bg-[#e97813] self-center font-bold text-[1.5rem] rounded-[0.3rem] border border-solid border-black text-black p-[1rem]'>start</span>  </div>
       </Link>
        
       
