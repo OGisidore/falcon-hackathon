a1a8aa123db0dd3f2484003b54478aaa009673ae
OGisidore <isidoreogoulodo43@gmail.com>
2024-08-06 21:20:11 +0100

    ..

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ interface HeaderProps {}
 
 const Header: FC<HeaderProps> = () => (
   <div className='min-w-[80%] flex justify-between p-[1rem] px-[2rem] '>
-    <div className="LOGO w-[10rem]"> <img className='w-[100%]'     src="assets/images/FindnSpeak.svg" alt="logo of fimd and speak" /></div>
+    <div className="LOGO md:w-[10rem] w-[6rem]"> <img className='w-[100%]'     src="assets/images/FindnSpeak.svg" alt="logo of fimd and speak" /></div>
     <LevelConponent/>
   </div>
 );
--- src/components/LevelConponent/LevelConponent.tsx
+++ src/components/LevelConponent/LevelConponent.tsx
@@ -5,13 +5,13 @@ interface LevelConponentProps {}
 
 const LevelConponent: FC<LevelConponentProps> = () => (
   <div className='   md:self-end flex flex-wrap gap-4 items-center '>
-    <div className="level font-sans text-[1.5rem]">
+    <div className="level font-sans text-[1rem] md:text-[1.5rem]">
       LEVEL : Beginer
     </div>
-    <div className="checked flex  w-[7rem]  ">
-      <img src="assets/images/yellow checkmark.svg" className='w-[2rem]' alt="checked-yellow" />
-      <img src="assets/images/gray checkmark.svg" className='w-[2rem]' alt="checked-yellow" />
-      <img src="assets/images/gray checkmark.svg" className='w-[2rem]' alt="checked-yellow" />
+    <div className="checked flex  md:w-[7rem] w-[4]  ">
+      <img src="assets/images/yellow checkmark.svg" className='md:w-[2rem] w[1rem]' alt="checked-yellow" />
+      <img src="assets/images/gray checkmark.svg" className='md:w-[2rem] w[1rem]' alt="checked-yellow" />
+      <img src="assets/images/gray checkmark.svg" className='md:w-[2rem] w[1rem]' alt="checked-yellow" />
     </div>
   </div>
 );
